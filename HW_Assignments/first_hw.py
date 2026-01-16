e1386bb0ec6b84b05e76c15745d1780d314a9f4e
Sidney Andreano <sidney122@comcast.net>
2026-01-16 17:43:17 +0000

    submit for review

---
+++ HW_Assignments/first_hw.py
@@ -0,0 +1,21 @@
+my_name = "Sidney Andreano"
+print(my_name)
+
+print("")
+
+print("Use 4 spaces per indentation level.")
+
+print("")
+
+print("Python has only one type of module object, and all modules are of this type, " 
+      "regardless of whether the module "
+      "is implemented in Python, C, or something else.\nTo help organize modules "
+      "and provide a naming hierarchy, Python has a concept of packages.\n"
+      "You can think of packages as the directories on a file system and modules as files within directories")
+
+print("")
+
+print("I hope to gain the ability to problem solve common computer science problems")
+print("I hope to gain more valueable experience in Python, and learn more internally how it works")
+print("I hope to gain more knowledge about the various libraries python has and why they are so useful")
+

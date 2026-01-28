08c341a086e3d0f2646d46e65228b0e05100a2b2
Sidney Andreano <sidney122@comcast.net>
2026-01-28 19:45:49 +0000

    in class

---
+++ In_class_assignments/math_func.py
@@ -0,0 +1,3 @@
+def add_multi(a, b):
+    add_multiply = (a + b) + (a * b)
+    return add_multiply
---
+++ In_class_assignments/tax_no_function.py
@@ -0,0 +1,30 @@
+## tax.no_function.py
+from math_func import add_multi
+
+
+
+print(add_multi(10, 20))
+# def calculate_price_with_tax():
+#     price = float(input('What is the price '))
+#     tax = float(input('What is the tax rate? '))
+#     calculated_price = price * (100 + tax) / 100
+#     print(f'The calculated price is {calculated_price}')
+# def get_inputs():
+#     done = False
+#     while not done:
+#         sentinel = str.upper(input(f'Enter Q for quit or any other key to compute tax '))
+#         if sentinel == 'Q':
+#             done = True
+#             print(sentinel,done)
+#             continue
+#         else:
+#             print("Compute tax")
+#             calculate_price_with_tax()
+
+
+
+# get_inputs()
+
+    
+
+

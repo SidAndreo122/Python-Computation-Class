cb9a1cb9f4c8615c62a59f8fa9d2c1aef995de3c
Sidney Andreano <sidney122@comcast.net>
2026-01-30 20:46:43 +0000

    in class

--- In_class_assignments/functional.py
+++ In_class_assignments/functional.py
@@ -1,9 +1,11 @@
-
-# map.grades.py
-# produce list of tuples
-grades = [95, 88, 85, 75]
-letter_grade = ['A', 'B+', 'B', 'C']
-print('The original list ',letter_grade)
-print('The zipped tuples ', list(zip(letter_grade, grades)))
-print('Next is a map-lambda version')
-map(lambda a: a, letter_grade, grades)  # equivalent to zip
+# conv_neg_pos_nocomp.py
+a_list = [7, 5, -4, 6]
+def check_negative(z):
+  result = []
+  for item in z:
+    if item < 0:
+      result.append(item * -1)
+      return result
+print(check_negative(a_list))
+a_list = [7, 5, -4, 6]
+print([-x for x in a_list if x < 0][:1])
